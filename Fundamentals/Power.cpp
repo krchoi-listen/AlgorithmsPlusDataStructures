06ca9cac858c20a36cd2072c1e8e4befef2ba9f8
krchoi-listen <45301931+krchoi-listen@users.noreply.github.com>
2022-01-03 11:38:00 -0500

    Init Project with Fundamental Power Function

---
+++ Fundamentals/Power.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+#include "Power.hpp"
+
+using namespace std;
+
+FundamentalPower::FundamentalPower()
+{
+
+}
+
+FundamentalPower::~FundamentalPower()
+{
+
+}
+
+void FundamentalPower::NegativePower(int n)
+{
+    int i, k, r;
+    vector<int> d;
+
+    for (k = 0; k < n; k++)
+    {
+        cout << ".";
+        r = 0;
+
+        for (i = 0; i < k; i++)
+        {
+            r = 10 * r + d[i];
+            d[i] = r / 2;
+            r = r - 2 * d[i];
+            cout << d[i];
+        }
+
+        d[k] = 5;
+        cout << "5" << endl;
+    }
+}
---
+++ Test/TestMain.cpp
@@ -0,0 +1,8 @@
+#include "../Fundamentals/Power.hpp"
+
+void main(void)
+{
+    FundamentalPower tempPower;
+
+    tempPower.NegativePower(10);
+}
