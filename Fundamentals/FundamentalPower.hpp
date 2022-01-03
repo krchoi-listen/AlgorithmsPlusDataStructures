eabebcff23ad7325cb25b085f0a3600dd8617496
krchoi-listen <45301931+krchoi-listen@users.noreply.github.com>
2022-01-03 11:54:06 -0500

    staging missing files, git commit -a command did not stage files in sub directory

---
+++ Fundamentals/FundamentalPower.hpp
@@ -0,0 +1,12 @@
+#ifndef __FUNDAMENTALS_POWER_HPP__
+#define __FUNDAMENTALS_POWER_HPP__
+#endif //__FUNDAMENTALS_POWER_HPP__
+
+class FundamentalPower
+{
+public:
+    FundamentalPower();
+    ~FundamentalPower();
+
+    void NegativePower(int n);
+};
